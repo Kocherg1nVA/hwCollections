6a9c56634f2c2a5ad4b4d19eaf6231efa6a373ca
Vadim Kochergin <kochergin1990vadim@gmail.com>
2024-12-25 19:52:29 +0300

    the task1 requirements are complited

--- src/main/java/ru/netology/Game.java
+++ src/main/java/ru/netology/Game.java
@@ -1,4 +1,38 @@
 package ru.netology;
 
+import java.util.ArrayList;
+
 public class Game {
+
+    private ArrayList<Player> players = new ArrayList<>();
+
+    public Player register(Player player) {
+        players.add(player);
+        return player;
+    }
+
+    public Player findByName(String name) {
+        for (Player player : players) {
+            if (player.getName().equals(name)) {
+                return player;
+            }
+        }
+        return null;
+    }
+
+    public int round(String playerName1, String playerName2) {
+        if (findByName(playerName1) == null) {
+            throw new NotRegisteredException("Player " + playerName1 + " is not registered");
+        } else if (findByName(playerName2) == null) {
+            throw new NotRegisteredException("Player" + playerName2 + " is not registered");
+        }
+
+        if (findByName(playerName1).getStrength() > findByName(playerName2).getStrength()) {
+            return 1;
+        } else if (findByName(playerName1).getStrength() < findByName(playerName2).getStrength()) {
+            return -1;
+        } else {
+            return 0;
+        }
+    }
 }
--- src/test/java/ru/netology/GameTest.java
+++ src/test/java/ru/netology/GameTest.java
@@ -3,6 +3,108 @@ package ru.netology;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
+import java.util.ArrayList;
+
 public class GameTest {
 
+    Player player1 = new Player(1, "Brut", 16);
+    Player player2 = new Player(12, "Lion", 18);
+    Player player3 = new Player(8, "Ozzy", 11);
+    Player player4 = new Player(8, "Zoltan", 16);
+
+    Game manager = new Game();
+
+    @Test
+    public void shouldAddPlayer() {
+
+        Assertions.assertEquals(player1, manager.register(player1));
+    }
+
+    @Test
+    public void shouldFindByName() {
+
+        manager.register(player1);
+        manager.register(player2);
+        manager.register(player3);
+        manager.register(player4);
+
+        Player expected = player2;
+        Player actual = manager.findByName("Lion");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldNotFindByName() {
+        manager.register(player1);
+        manager.register(player2);
+        manager.register(player3);
+
+        Player expected = null;
+        Player actual = manager.findByName("Zoltan");
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPlayer2Wins() {
+        manager.register(player1);
+        manager.register(player2);
+
+        int expected = -1;
+        int actual = manager.round(player1.getName(), player2.getName());
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldPlayer1Wins() {
+        manager.register(player1);
+        manager.register(player3);
+
+        int expected = 1;
+        int actual = manager.round(player1.getName(), player3.getName());
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldBeDraw() {
+        manager.register(player1);
+        manager.register(player4);
+
+        int expected = 0;
+        int actual = manager.round(player1.getName(), player4.getName());
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldThrowWhenPlayer1NotRegistered() {
+        manager.register(player2);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> {
+                    manager.round(player1.getName(), player2.getName());
+                });
+    }
+
+    @Test
+    public void shouldThrowWhenPlayer2NotRegistered() {
+        manager.register(player1);
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> {
+                    manager.round(player1.getName(), player2.getName());
+                });
+    }
+
+    @Test
+    public void shouldThrowWhenAllPlayersIsNotRegistered() {
+
+        Assertions.assertThrows(NotRegisteredException.class,
+                () -> {
+                    manager.round(player3.getName(), player4.getName());
+                });
+    }
 }
